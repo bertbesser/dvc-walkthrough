71c5b9df1f1710a26391384587424cc9f343a800
bbesser <bert.besser@codecentric.de>
2019-02-27 11:23:17 +0100

    comment tutorial commands

--- code/tutorial_commands.sh
+++ code/tutorial_commands.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-git status
-git init
+git status # this is not a git repo yet
+git init # let's make it a git repo
 git config user.email "you@example.com"
 git config user.name "Your Name"
-git add .
+git add . # prepare initial import
 git commit -m "initial import"
 git status
-dvc init
+dvc init # init dvc
 git status
-git add .
+git add . # add all dvc core files
 git commit -m "init dvc"
-dvc run -f loaddata.dvc -o /blog-dvc/data python /blog-dvc/code/load_data.py 0.01
+dvc run -f loaddata.dvc -o /blog-dvc/data python /blog-dvc/code/load_data.py 0.01 # run load data for version 0.01
 echo data >> .gitignore # this folder will be managed by dvc, git can safely ignore this folder
 git add loaddata.dvc .gitignore
 git commit -m "0.01 load data"
