3e612eb9e0724b5ab765c871963b70af4493ea3e
Bert Besser <bert.besser@codecentric.de>
2019-10-26 11:19:09 +0200

    rename team members to fit team foto

--- scripts/livedemo.sh
+++ scripts/livedemo.sh
@@ -4,24 +4,24 @@
 # PART I create pipeline
 ######
 
-# bob checks out alice's code
+# vince checks out dan's code
 git clone git@github.com:bbesser/dvc-livedemo.git livedemo
 cd livedemo
 ls
 
-# bob creates the pipeline configuration (versioned in git)
+# vince creates the pipeline configuration (versioned in git)
 mkdir config
 echo '{ "num_images" : 1000 }' > config/load.json
 echo '{ "num_conv_filters" : 32 }' > config/train.json
 git add config/load.json config/train.json
 git commit -m "create pipeline configuration"
 
-# bob runs the pipeline
+# vince runs the pipeline
 ./run_pipeline.sh
 du -h
 git status
 
-# bob prepares the pipeline setup
+# vince prepares the pipeline setup
 dvc init
 git status
 dvc remote add -d bertsBucket s3://dvc-livedemo.bertatcodecentric.de/dvc-livedemo
@@ -31,7 +31,7 @@ git commit -m "init dvc"
 git tag -a 0.0 -m "0.0 freshly initialized project with no pipeline defined, yet"
 git status
 
-# bob creates the dvc pipeline stages
+# vince creates the dvc pipeline stages
 git rm run_pipeline.sh
 dvc run -f load.dvc -d config/load.json -o data python code/load.py
 dvc run -f train.dvc -d data -d config/train.json -o model.h5 python -B code/train.py
@@ -40,15 +40,15 @@ git status
 cat .gitignore
 
 git add *.dvc metrics.json .gitignore
-git commit -m "create dvc stages for alice's pipeline"
+git commit -m "create dvc stages for dan's pipeline"
 
-# bob inspects metrics
+# vince inspects metrics
 # // just for the sake of completeness - will not be discussed further
 # // - dvc can format metrics
 # // - dvc can compare metrics of different pipeline versions
 dvc metrics show
 
-# bob tags the initial pipeline
+# vince tags the initial pipeline
 echo "
 To reproduce, \`git checkout\` a tag and then \`dvc repro evaluate.dvc\`.
 " > README.md
@@ -61,7 +61,7 @@ git push origin master 0.0 0.1
 # PART II develop and reproduce pipeline
 ######
 
-# alice reproduces the pipeline (partially)
+# dan reproduces the pipeline (partially)
 git clone git@github.com:bbesser/dvc-livedemo.git livedemo
 cd livedemo
 git checkout 0.1
@@ -71,10 +71,10 @@ ll
 dvc repro evaluate.dvc
 ll
 
-# alice does not have to repro
+# dan does not have to repro
 dvc repro evaluate.dvc
 
-# alice improves the training configuration
+# dan improves the training configuration
 git checkout master
 git reset --hard HEAD
 echo '{ "num_conv_filters" : 64 }' > config/train.json
@@ -92,21 +92,20 @@ git push origin master 0.2
 # PART III share with team
 ######
 
-# chris wants to pick up on the team's work
+# clair wants to pick up on the team's work
 
-# bob shares artifacts for 0.1
+# vince shares artifacts for 0.1
 git pull
 git checkout 0.1
 dvc push
 
-# alice imitates bob
+# dan imitates vince
 git checkout 0.2
 dvc push
 
-# chris continues their work
+# clair continues their work
 git clone git@github.com:bbesser/dvc-livedemo.git livedemo
 cd livedemo
-git tag
 git checkout 0.1
 ll
 dvc pull # fetches training images and model
@@ -120,12 +119,12 @@ dvc repro evaluate.dvc
 # PART IV extend pipeline (optional)
 ######
 
-# chris implements model conversion
+# clair implements model conversion
 git checkout master
 git reset --hard HEAD
 cp /repo/code/publish.py code
 
-# chris creates the publish stage
+# clair creates the publish stage
 dvc run -f publish.dvc -d model.h5 -o model.onnx python code/publish.py
 git add code/publish.py publish.dvc .gitignore
 git commit -m 'create publish stage (to onnx format)'
@@ -133,7 +132,7 @@ git tag -a 0.3 -m "0.3 publish to onnx"
 git push origin master 0.3
 dvc push
 
-# alice inspects chris's work
+# dan inspects clair's work
 git checkout master
 git reset --hard HEAD
 git pull
